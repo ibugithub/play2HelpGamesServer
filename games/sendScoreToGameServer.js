e27a3f25ea2268c900dfbed4335bc844b18fe922
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-12-03 00:43:47 +0600

    tring to fix the score system

--- games/sendScoreToGameServer.js
+++ games/sendScoreToGameServer.js
@@ -1,9 +1,9 @@
 export const sendScore = async(score) => {
   // const urlParams = new URLSearchParams(window.location.search);
   // const accessToken =  urlParams.get('to');
-  const SELF_BASE_URL = 'http://localhost:3003';
+  // const SELF_BASE_URL = 'http://localhost:3003';
   // console.log('the accessToken in the sendScore is', accessToken);
-  // const SELF_BASE_URL = 'https://play2helpgamesserver.onrender.com'
+  const SELF_BASE_URL = 'https://play2helpgamesserver.onrender.com'
   const subUri ='api/sendScoreToDB'
   try {
     await fetch(`${SELF_BASE_URL}/${subUri}`, {
