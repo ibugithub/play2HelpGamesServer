a7cdeb629735fde492f2c68902399bc694b9dccb
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-12-02 23:52:31 +0600

    remve all the extra session burden and pass the token directly

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -1,22 +1,25 @@
 import axios from "axios";
 
 export const sendScoreFromGameEngine = async (req, res) => {
+  console.log('I am in the sendScoreFromGameEngine');
   const { score } = req.body;
   console.log('the score is', score)
+  console.log('the req obj is', req);
   return res.json({ message: 'Score sent successfully' });
 };
 
 
 export const sendScoreToDB = async (req, res) => {
-  const { score, accessToken } = req.body;
-  console.log('the accessToken in the sendScoreToDB is', accessToken);
-  if (accessToken && accessToken !== 'null') { 
-    const subUri = 'api/games/submitScore/';
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/${subUri}`;
-    axios.post(url, { score: score }, { headers: { Authorization: `Bearer ${accessToken}` } });
-  }
-  else {
-    console.log("No access token found in local storage");
-  }
+  console.log('the req  in sendScore is', req);
+  const { score } = req.body;
+  // console.log('the accessToken in the sendScoreToDB is', accessToken);
+  // if (accessToken && accessToken !== 'null') { 
+  //   const subUri = 'api/games/submitScore/';
+  //   const url = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/${subUri}`;
+  //   axios.post(url, { score: score }, { headers: { Authorization: `Bearer ${accessToken}` } });
+  // }
+  // else {
+  //   console.log("No access token found in local storage");
+  // }
   return res.json({ message: 'Score sent successfully' });
 };
--- games/sendScoreToGameServer.js
+++ games/sendScoreToGameServer.js
@@ -1,8 +1,8 @@
 export const sendScore = async(score) => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const accessToken =  urlParams.get('to');
+  // const urlParams = new URLSearchParams(window.location.search);
+  // const accessToken =  urlParams.get('to');
   // const SELF_BASE_URL = 'http://localhost:3003';
-  console.log('the accessToken in the sendScore is', accessToken);
+  // console.log('the accessToken in the sendScore is', accessToken);
   const SELF_BASE_URL = 'https://play2helpgamesserver.onrender.com'
   const subUri ='api/sendScoreToDB'
   try {
@@ -12,7 +12,7 @@ export const sendScore = async(score) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ score, accessToken }),
+      body: JSON.stringify({ score }),
     });
   } catch (error) {
     console.error('Failed to send score:', error);
