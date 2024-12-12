0340bfc2e0263ab021a7cba71942f470f28c8e39
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-12-12 11:31:13 +0600

    Change name to tokens of rewards

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -5,11 +5,11 @@ export const sendScoreToDB = async (req, res) => {
   const url = new URL(referer);
   const accessToken = url.searchParams.get('to');
   const { score, gameName } = req.body;
-  const reward = score * 0.0002;
+  const tokens = score * 0.0002;
   if (accessToken && accessToken !== 'null') { 
     const subUri = 'api/games/submitScore/';
     const url = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/${subUri}`;
-    axios.post(url, { score: score, game: gameName, reward }, { headers: { Authorization: `Bearer ${accessToken}` } });
+    axios.post(url, { score: score, game: gameName, tokens }, { headers: { Authorization: `Bearer ${accessToken}` } });
   }
   else {
     console.log("No access token found in local storage");
