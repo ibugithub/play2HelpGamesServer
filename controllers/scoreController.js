69637f1e9ce6a9a5d2c721853964e39b3a38f22c
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2025-12-15 23:30:40 +0600

    added test log for origin and referer

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const sendScoreToDB = async (req, res) => {
+  const origin = req.get('Origin');
+  console.log('Request origin:', origin);
   const referer = req.get('Referer');
+  console.log('Request referer:', referer);
   const url = new URL(referer);
   const accessToken = url.searchParams.get('to');
   const { score, gameName } = req.body;
